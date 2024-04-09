060e39a7689d668f9c1f5237e4ee90bc55fd41d5
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-04-09 20:24:19 +0530

    new

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,13 +5,7 @@ const Sidebar = () => {
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
       <div className="flex flex-col gap-6">
         {
-          [
-            {
-              imgUrl: '/icons/Home.svg',
-              route:'/',
-              label:'Home'
-            }
-          ]
+          []
         }
       </div>
     </section>
