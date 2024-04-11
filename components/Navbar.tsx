c3c0ab2734f97b3880cbbbfed85fe1e40001e40f
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-04-11 10:26:33 +0530

    20240411-1026

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import MobileNav from './MobileNav'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="boom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          BOOM
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn> */}
+
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,36 @@
 export const sidebarLinks = [
   {
-    label: 'Home',
+    imgURL: '/icons/Home.svg',
     route: '/',
-    imgUrl: '/icons/Home.svg',
+    label: 'Home',
   },
+
   {
-    label: 'Upcoming',
+    imgURL: '/icons/upcoming.svg',
     route: '/upcoming',
-    imgUrl: '/icons/upcoming.svg',
+    label: 'Upcoming',
   },
   {
-    label: 'Previous',
+    imgURL: '/icons/previous.svg',
     route: '/previous',
-    imgUrl: '/icons/previous.svg',
+    label: 'Previous',
   },
   {
-    label: 'Recordings',
+    imgURL: '/icons/Video.svg',
     route: '/recordings',
-    imgUrl: '/icons/Video.svg',
+    label: 'Recordings',
   },
   {
-    label: 'Personal Room',
+    imgURL: '/icons/add-personal.svg',
     route: '/personal-room',
-    imgUrl: '/icons/add-personal.svg',
+    label: 'Personal Room',
   },
+];
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
 ];
