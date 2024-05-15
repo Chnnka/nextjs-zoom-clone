4dff47b317ebb6a4f0ef981205175ec0abecd6c6
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-05-15 16:45:24 +0530

    dialog box initial

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,19 +1,52 @@
 import React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 interface MeetingModalProps{
   isOpen:boolean;
   onClose:()=>void;
   title:string;
-  className:string;
+  className?:string;
   buttonText:string;
   image:string;
   buttonIcon:string;
-  children:string;
-  handleClick:()=>{};
+  children?:string;
+  handleClick: () => void;
 }
-const MeetingModal = ({isOpen,onClose,title,className,children,handleClick,buttonText,image,buttonIcon}) => {
+const MeetingModal = (
+  {
+    isOpen,
+    onClose,
+    title,
+    className,
+    children,
+    handleClick,
+    buttonText,
+    image,
+    buttonIcon
+  } : MeetingModalProps
+) => {
   return (
-    <div>MeetingModal</div>
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+  </Dialog>
+
   )
 }
 
